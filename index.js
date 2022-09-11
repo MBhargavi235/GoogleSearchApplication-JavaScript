41e0a1d358edf81daedacd0838f21245069434f7
MBhargavi235 <106804698+MBhargavi235@users.noreply.github.com>
2022-09-11 19:02:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+let searchInput=document.getElementById("searchInput");
+let spinnerEl = document.getElementById("spinner");
+searchInput.addEventListener("keydown",function(event){
+    spinnerEl.classList.remove("d-none");
+    if(event.key==="Enter"){
+    console.log(searchInput.value);
+    let url="https://apis.ccbp.in/wiki-search?search="+searchInput.value;
+    console.log(url);
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let output="";
+        for(let item of data.search_results){
+        console.log(item);
+        output+=`
+        <div>
+         <p>${item.title}</p>
+         <a href="${item.link}">${item.link}</a>
+         <p>${item.description}</p>
+        </div>
+        `
+        }
+        document.querySelector(".search-results").innerHTML=output;
+        searchInput.value="";
+         spinnerEl.classList.add("d-none");
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+})
